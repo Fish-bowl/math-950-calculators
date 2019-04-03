ddc1cf726eb1130397b4238cbe5569aefa279e3d
Fish-bowl <symbolt.ap@gmail.com>
2019-04-02 19:55:06 -0600

    got rid of unused vars

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Divider, Segment } from 'semantic-ui-react';
+import {Divider, Segment } from 'semantic-ui-react';
 import Calculators from './Calculators';
 import Intro from './Intro';
 import Wolfram from './Wolfram';
